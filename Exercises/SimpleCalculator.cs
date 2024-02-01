0e0726b94bf2104d98d46b3e9c1fab9767b741ef
Daniel Chaves <danielchavesfa@hotmail.com>
2024-02-01 16:26:55 -0300

    chore: calculator implementation begins

--- Exercises/SimpleCalculator.cs
+++ Exercises/SimpleCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,27 @@ using System.Threading.Tasks;
 
 namespace Challenges.Exercises {
     internal class SimpleCalculator {
+        private static void RenderAsciiArt() {
+            Console.WriteLine(
+                    " ________  ________  ___       ________     \r\n" +
+                    "|\\   ____\\|\\   __  \\|\\  \\     |\\   ____\\    \r\n" +
+                    "\\ \\  \\___|\\ \\  \\|\\  \\ \\  \\    \\ \\  \\___|    \r\n" +
+                    " \\ \\  \\    \\ \\   __  \\ \\  \\    \\ \\  \\       \r\n" +
+                    "  \\ \\  \\____\\ \\  \\ \\  \\ \\  \\____\\ \\  \\____  \r\n" +
+                    "   \\ \\_______\\ \\__\\ \\__\\ \\_______\\ \\_______\\\r\n" +
+                    "    \\|_______|\\|__|\\|__|\\|_______|\\|_______|");
+        }
+
         public static void Init() {
-            Console.WriteLine("Iniciou o exercício da Calculadora Simples");
+            RenderAsciiArt();
+
+            Console.WriteLine(
+                "Qual operação gostaria de fazer?\n" +
+                "1 - Somar\n" +
+                "2 - Subtrair\n" +
+                "3 - Multiplicar\n" +
+                "4 - Dividir\n" +
+                "5 - Sair");
         }
     }
 }
